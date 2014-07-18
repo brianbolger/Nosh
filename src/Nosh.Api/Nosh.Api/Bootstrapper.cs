d0d12f85cc688c6b1bcf5f3e973d6413413a59d8
Brian Bolger <brianjamesbolger@gmail.com>
2014-07-18 17:05:58 +0100

    Bootstrapping RavenDB into the Nancy module

---
+++ src/Nosh.Api/Nosh.Api/Bootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using Nancy;
+using Nancy.TinyIoc;
+using Raven.Client;
+using Raven.Client.Document;
+
+namespace Nosh.Api
+{
+	public class Bootstrapper : DefaultNancyBootstrapper
+	{
+		protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+		{
+			base.ConfigureApplicationContainer(container);
+
+			var documentStore = new DocumentStore
+				{
+					ConnectionStringName = "NoshDB"
+				};
+
+			documentStore.Initialize();
+			container.Register(documentStore, "DocumentStore");
+		}
+
+		protected override void ConfigureRequestContainer(TinyIoCContainer container, NancyContext context)
+		{
+			base.ConfigureRequestContainer(container, context);
+
+			var docStore = container.Resolve<DocumentStore>("DocumentStore");
+			var documentSession = docStore.OpenSession();
+			container.Register<IDocumentSession>(documentSession);
+		}
+	}
+}
