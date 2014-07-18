90585f2a74c3b18dd0875cb09594e6349971cc81
Brian Bolger <brianjamesbolger@gmail.com>
2014-07-18 17:07:50 +0100

    Bootstrapping RavenDB into the nancy module

--- src/Nosh.Api/Nosh.Api/Modules/IndexModule.cs
+++ src/Nosh.Api/Nosh.Api/Modules/IndexModule.cs
@@ -1,11 +1,16 @@
 ﻿using Nancy;
+using Raven.Client;
 
 namespace Nosh.Api.Modules
 {
 	public class IndexModule : NancyModule
 	{
-		public IndexModule()
+		private IDocumentSession DocumentSession { get; set; }
+
+		public IndexModule(IDocumentSession documentSession)
 		{
+			DocumentSession = documentSession;
+
 			Get["/"] = p => "Nosh API root";
 		}
 	}
