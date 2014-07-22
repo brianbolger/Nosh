d9a612835d615f2dc9be21fa27560ea2e3c6d865
Brian Bolger <brianjamesbolger@gmail.com>
2014-07-22 16:51:10 +0100

    Added domain classes & basic GET request handlers.

--- src/Nosh.Api/Nosh.Api/Modules/IndexModule.cs
+++ src/Nosh.Api/Nosh.Api/Modules/IndexModule.cs
@@ -1,4 +1,7 @@
-﻿using Nancy;
+﻿using System;
+using System.Linq;
+using Nancy;
+using Nosh.Api.Model;
 using Raven.Client;
 
 namespace Nosh.Api.Modules
@@ -12,6 +15,51 @@ namespace Nosh.Api.Modules
 			DocumentSession = documentSession;
 
 			Get["/"] = p => "Nosh API root";
+
+			Get["/locations"] = p => GetLocations();
+			Get["/locations/{location}"] = p => GetLocationByName(p.location.ToString());
+			
+			//Get["/locations/{location}/dailyorders"] = p => ...
+			// have an overload querystring to specify the date?
+
+			Get["/menus"] = p => GetMenus();
+			Get["/menus/{menu}"] = p => GetMenuByName(p.menu.ToString());
+
+			Get["/users/{user}"] = p => GetUserByName(p.user.ToString());
+			Get["/users/{user}/orders"] = p => GetOrdersByUser(p.user.ToString());
+		}
+
+		private Response GetLocations()
+		{
+			return Response.AsJson(DocumentSession.Query<Location>());
+		}
+
+		private Response GetLocationByName(string locationName)
+		{
+			var location = DocumentSession.Query<Location>().FirstOrDefault(l => l.Name.Equals(locationName, StringComparison.OrdinalIgnoreCase));
+			return location == null ? HttpStatusCode.NotFound : Response.AsJson(location);
+		}
+
+		private Response GetMenus()
+		{
+			return Response.AsJson(DocumentSession.Query<Menu>());
+		}
+
+		private Response GetMenuByName(string menuName)
+		{
+			var menu = DocumentSession.Query<Menu>().FirstOrDefault(l => l.Name.Equals(menuName, StringComparison.OrdinalIgnoreCase));
+			return menu == null ? HttpStatusCode.NotFound : Response.AsJson(menu);
+		}
+
+		private Response GetUserByName(string userName)
+		{
+			var user = DocumentSession.Query<User>().FirstOrDefault(l => l.Name.Equals(userName, StringComparison.OrdinalIgnoreCase));
+			return user == null ? HttpStatusCode.NotFound : Response.AsJson(user);
+		}
+
+		private Response GetOrdersByUser(string userName)
+		{
+			return Response.AsJson(DocumentSession.Query<Order>().Where(o => o.UserName.Equals(userName, StringComparison.OrdinalIgnoreCase)));
 		}
 	}
 }
